0dc08b4162709adb7a2c0c79943ed7c5b6e926a9
Tomás Mezzabotta <tomasmezzabotta06@gmail.com>
2025-06-25 12:21:15 -0300

    Cambio de modelo

--- prod/app.py
+++ prod/app.py
@@ -15,7 +15,14 @@ from model import FERModel  # Asegurate de tener esta clase en model.py
 
 # ========= MODELO Y CONFIG =========
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = FERModel()
+def resnet18_model(num_classes=7, dropout_p=0.6):
+    model = resnet18(weights=ResNet18_Weights.DEFAULT)
+    model.fc = nn.Sequential(
+        nn.Dropout(p=dropout_p),
+        nn.Linear(512, num_classes)
+    )
+    return model
+model = resnet18_model()
 model_path = "modelo_entrenado.pth"
 url = "https://raw.githubusercontent.com/TotoMezza/Emocion/main/dev/modelo_entrenado.pth"
 
