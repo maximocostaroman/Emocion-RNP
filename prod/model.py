ff24da12dff244c4a5c3af19c937eeb51e752e43
Tomás Mezzabotta <tomasmezzabotta06@gmail.com>
2025-06-26 19:09:23 -0300

    Hermoso

--- prod/model.py
+++ prod/model.py
@@ -1,9 +1,10 @@
 from torchvision.models import resnet18, ResNet18_Weights
 import torch.nn as nn
 
-def resnet18_model(num_classes=7, dropout_p=0.6):
+def resnet18_model(num_classes=7, dropout_p=0.7):
     model = resnet18(weights=ResNet18_Weights.DEFAULT)
     model.fc = nn.Sequential(
+        nn.BatchNorm1d(512),
         nn.Dropout(p=dropout_p),
         nn.Linear(512, num_classes)
     )
